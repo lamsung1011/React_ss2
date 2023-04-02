2bd8f560b599ce7cb3f747624454717439c049e0
lamsung1011 <nguyen.siege@gmail.com>
2023-04-03 01:38:11 +0700

    variables

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let name = 'Mù';
+  let obj = { name: 'Mù', age: 18, truename: 'Công Thành' }
+
   return (
+
     <div className="App">
+      {console.log('check obj', obj)}
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Hello world - React from {name}</h1>
+        <p>This is {obj.name} {obj.age} tủi</p>
+        <a href='https://www.facebook.com/nguyen.congthanh.0111/'>Visit my facebook</a>
         <a
           className="App-link"
           href="https://reactjs.org"
