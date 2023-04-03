5523c4514f0f20d943317865748578cfc5952bf9
lamsung1011 <nguyen.siege@gmail.com>
2023-04-03 15:09:11 +0700

    video 10

--- src/App.js
+++ src/App.js
@@ -1,28 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
+import Nav from './views/Nav';
+import { useState } from 'react';
 
-function App() {
 
-  let name = 'Mù';
-  let obj = { name: 'Mù', age: 18, truename: 'Công Thành' }
+const App = () => {
+
+  let [name, setName] = useState('Muf')
+  // let obj = { name: 'Mù', age: 18, truename: 'Công Thành' }
+  const [address, setAddress] = useState('');
+  const [todos, setTodos] = useState([
+    { id: 'todo1', title: 'Coding' },
+    { id: 'todo2', title: 'Gaming' },
+    { id: 'todo3', title: 'Fucking' }
+  ]);
+
+  const handleEventClick = (event) => {
+
+    if (!address) {
+      alert('Điền vào hộ bố mày cái')
+      return;
+    }
+
+    let newTodo = { id: 'abc', title: address }
+    setTodos([...todos, newTodo])
+    setAddress('')
+  }
+
+  const handleOnchangeInput = (event) => {
+    setAddress(event.target.value)
+    // console.log(event.target.value);
+  }
 
   return (
 
     <div className="App">
-      {console.log('check obj', obj)}
+      <Nav />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello world - React from {name}</h1>
-        <p>This is {obj.name} {obj.age} tủi</p>
-        <a href='https://www.facebook.com/nguyen.congthanh.0111/'>Visit my facebook</a>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className='todo-container'>
+          {todos.map(todo => {
+            console.log('>>>check todo list', todo);
+            return (
+              <li className='todo-child' key={todo.id}> {todo.title} </li>
+            )
+          })}
+        </div>
+        {/* <p>This is {obj.name} {obj.age} tủi</p> */}
+        <input type='text' value={address} onChange={(event) => handleOnchangeInput(event)} />
+        <button type='button' onClick={(event) => handleEventClick(event)}>Click me</button>
       </header>
     </div>
   );
