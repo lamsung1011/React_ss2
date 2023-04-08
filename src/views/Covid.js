b068bd0bb8953d84cee36bb428a8232de7b0b97d
lamsung1011 <nguyen.siege@gmail.com>
2023-04-08 16:38:59 +0700

    handle error fetching data

--- src/views/Covid.js
+++ src/views/Covid.js
@@ -7,22 +7,35 @@ const Covid = () => {
 
 
     const [dataCovid, setDataCovid] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
+    const [isError, setIsError] = useState(false);
 
     useEffect(async () => {
-        let res = await axios.get('https://api.covid19api.com/country/vietnam?from=2021-10-01T00:00:00Z&to2021-10-20T00:00:00Z')
-        let data = res && res.data ? res.data : [];
-        if (data && data.length > 0) {
-            data.map(item => {
-                item.Date = moment(item.Date).format('DD/MM/YYYY')
-                return item;
-            })
+        try {
+            // setTimeout(async () => {
+            let res = await axios.get('https://api.covid19api.com/country/vietnam?from=2021-10-01T00:00:00Z&to2021-10-20T00:00:00Z')
+            let data = res && res.data ? res.data : [];
+            if (data && data.length > 0) {
+                data.map(item => {
+                    item.Date = moment(item.Date).format('DD/MM/YYYY')
+                    return item;
+                })
+                data = data.reverse()
+            }
+            setDataCovid(data);
+            setIsLoading(false);
+            setIsError(false)
+
+            // }, 3500)
+        } catch (e) {
+            setIsError(true);
+            setIsLoading(false);
         }
-        setDataCovid(data)
     }, []);
 
     return (
         <>
-            <h2>Covid 19 Tracking in Việt Nam</h2 >
+            <h2>Covid-19 Tracking in Việt Nam</h2 >
             < table >
                 {console.log('check data covid', dataCovid)}
                 <thead>
@@ -35,17 +48,25 @@ const Covid = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    {dataCovid && dataCovid.length > 0 && dataCovid.map(item => {
-                        return (
-                            <tr key={item.ID}>
-                                <td>{item.Date}</td>
-                                <td>{item.Confirmed}</td>
-                                <td>{item.Active}</td>
-                                <td>{item.Deaths}</td>
-                                <td>{item.Recovers}</td>
-                            </tr>
-                        )
-                    })}
+
+
+
+                    {
+                        isError === false && isLoading === false && dataCovid && dataCovid.length > 0 && dataCovid.map(item => {
+                            return (
+                                <tr key={item.ID}>
+                                    <td>{item.Date}</td>
+                                    <td>{item.Confirmed}</td>
+                                    <td>{item.Active}</td>
+                                    <td>{item.Deaths}</td>
+                                    <td>{item.Recovers}</td>
+                                </tr>
+                            )
+                        })
+                    }
+                    {isLoading === true && <tr><td colSpan='5' style={{ 'textAlign': 'center' }}>Loading...</td></tr>}
+
+                    {isError === true && <tr><td colSpan='5' style={{ 'textAlign': 'center' }}>Something wrong !!!</td></tr>}
 
                 </tbody>
             </table >
